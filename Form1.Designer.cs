f43ff455d04bc01086dc3674bbf0be1fee6c5d37
Gershon Naidoo <Nukphonik@gmail.com>
2020-05-14 04:31:26 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace MyOriginalLottoCombinations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Load = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Compare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(45, 44);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 0;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(137, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(137, 181);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Previous Winning Combinations";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Your Winning Combinations";
+            // 
+            // Compare
+            // 
+            this.Compare.Location = new System.Drawing.Point(45, 94);
+            this.Compare.Name = "Compare";
+            this.Compare.Size = new System.Drawing.Size(75, 23);
+            this.Compare.TabIndex = 5;
+            this.Compare.Text = "Compare";
+            this.Compare.UseVisualStyleBackColor = true;
+            this.Compare.Click += new System.EventHandler(this.Compare_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 290);
+            this.Controls.Add(this.Compare);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Load);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "My Original non drawn Lotto Combinations";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Compare;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyOriginalLottoCombinations
+{
+    public partial class Form1 : Form
+    {
+        int recordcount, recordcounter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Compare_Click(object sender, EventArgs e)
+        {
+            StreamReader original = new StreamReader("DailyLotto.txt");
+            StreamReader MySeq = new StreamReader("ComSeq.txt");
+            string z, y, searching;
+            string[] x = new string[recordcounter];
+            string[] t = new string[recordcount];
+            int mycount, mycounter;
+
+
+            for (mycount = 0; mycount < recordcount; mycount++)
+            {
+                t[mycount] = MySeq.ReadLine();
+            }
+            MySeq.Close();
+            MySeq.Dispose();
+            for (mycounter = 0; mycounter < recordcounter; mycounter++)
+            {
+                x[mycounter] = original.ReadLine();
+
+            }
+            original.Close();
+            original.Dispose();
+
+            for (mycount = 0; mycount < recordcount; mycount++)
+            {
+                for (mycounter = 0; mycounter < recordcounter; mycounter++)
+                {
+                    if (x[mycounter] == t[mycount])
+                    {
+                        MessageBox.Show(x[mycounter], " Already won");
+                    }
+                }
+            }
+        }
+
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("DailyLotto.txt");
+            StreamReader SR = new StreamReader("ComSeq.txt");
+            string x, y;
+            x = sr.ReadLine();
+            y = SR.ReadLine();
+            recordcount = 0;
+            recordcounter = 0;
+
+
+            while (y != null)
+            {
+                listBox2.Items.Add(y);
+                y = SR.ReadLine();
+                recordcount = recordcount + 1;
+            }
+
+            while (x != null)
+            {
+                listBox1.Items.Add(x);
+                x = sr.ReadLine();
+                recordcounter = recordcounter + 1;
+            }
+
+        }
+    }
+}
